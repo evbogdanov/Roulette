55166a4ff743e3719bcf15f1e5eebce9897b0f45
Ev Bogdanov <ev@bogdanov.io>
2017-04-03 14:53:54 +0300

    first vc

---
+++ Roulette/ViewControllers/RouletteViewController.m
@@ -0,0 +1,22 @@
+//
+//  RouletteViewController.m
+//  Roulette
+//
+//  Created by Ev Bogdanov on 03/04/2017.
+//  Copyright © 2017 Ev Bogdanov. All rights reserved.
+//
+
+#import "RouletteViewController.h"
+
+@interface RouletteViewController ()
+
+@end
+
+@implementation RouletteViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"It works");
+}
+
+@end
